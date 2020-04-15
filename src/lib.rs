585676494b44686fb3c8eef4d3bc44aab1cb6c85
Anshul Goyal <anshulgoel151999@gmail.com>
2020-04-16 03:00:01 +0530

    First Code Push

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ pub unsafe extern "C" fn napi_register_module_v1(
     env: napi_env,
     exports: napi_value,
 ) -> nodejs_sys::napi_value {
-    let key = CString::new("hello").expect("CString::new failed");
-    let mut local: napi_value = std::mem::zeroed();
-    let value = CString::new("world!").expect("CString::new failed");
-    napi_create_string_utf8(env, value.as_ptr(), 6, &mut local);
-    napi_set_named_property(env, exports, key.as_ptr(), local);
+    // let key = CString::new("hello").expect("CString::new failed");
+    // let mut local: napi_value = std::mem::zeroed();
+    // let value = CString::new("world!").expect("CString::new failed");
+    // napi_create_string_utf8(env, value.as_ptr(), 6, &mut local);
+    // napi_set_named_property(env, exports, key.as_ptr(), local);
     exports
 }
